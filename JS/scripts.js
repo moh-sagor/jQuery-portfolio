ae1377a7e98a1c0bdd0942feb45ebf54178e1d59
Sagor Hossain <sagorhassain4@gmail.com>
2023-02-17 21:27:06 +0600

    update details

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,54 +1,54 @@
 jQuery(document).ready(function () {
 
     jQuery(".home").click(function () {
-        jQuery(".s-about").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-service").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-about").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-service").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
         jQuery(".s-home").animate({ left: '46%' }, 10).css({ 'width': '45.5%', "opacity": '1', 'transition': '.5s' });
-        jQuery(".s-portfolio").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-testimonial").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-gallery").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-portfolio").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-testimonial").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-gallery").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
     });
     jQuery(".about").click(function () {
         jQuery(".s-about").animate({ left: '46%' }, 10).css({ 'width': '45.5%', "opacity": '1', 'transition': '.5s' });
-        jQuery(".s-home").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-service").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-portfolio").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-gallery").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-testimonial").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-home").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-service").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-portfolio").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-gallery").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-testimonial").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
     });
 
     jQuery(".service").click(function () {
         jQuery(".s-service").animate({ left: '46%' }, 10).css({ 'width': '45.5%', "opacity": '1', 'transition': '.5s' });
-        jQuery(".s-about").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-home").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-portfolio").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-testimonial").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-gallery").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-about").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-home").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-portfolio").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-testimonial").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-gallery").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
     });
     jQuery(".portfolio").click(function () {
         jQuery(".s-portfolio").animate({ left: '46%' }, 10).css({ 'width': '45.5%', "opacity": '1', 'transition': '.5s' });
-        jQuery(".s-about").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-service").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-gallery").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-home").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-testimonial").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-about").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-service").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-gallery").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-home").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-testimonial").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
     });
     jQuery(".testimonial").click(function () {
         jQuery(".s-testimonial").animate({ left: '46%' }, 10).css({ 'width': '45.5%', "opacity": '1', 'transition': '.5s' });
-        jQuery(".s-portfolio").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-about").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-service").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-gallery").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-home").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-portfolio").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-about").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-service").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-gallery").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-home").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
 
     });
     jQuery(".gallery").click(function () {
         jQuery(".s-gallery").animate({ left: '46%' }, 10).css({ 'width': '45.5%', "opacity": '1', 'transition': '.5s' });
-        jQuery(".s-about").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-home").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-service").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-portfolio").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
-        jQuery(".s-testimonial").animate({ right: '8%' }, 10).css({ 'width': '20%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-about").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-home").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-service").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-portfolio").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
+        jQuery(".s-testimonial").animate({ right: '1%' }, 10).css({ 'width': '1%', "opacity": '0', 'transition': '.5s' });
     });
 
     var width = jQuery(this).width();
